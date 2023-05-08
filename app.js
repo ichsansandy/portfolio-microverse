a61ec6ba86a8f12102bdd9a7a44f57487cad6c09
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-05-08 13:35:46 +0700

    Animate X icon for the menu mobile

--- app.js
+++ app.js
@@ -8,13 +8,16 @@ window.addEventListener('DOMContentLoaded', () => {
       const menuVisibility = primaryNavigation.getAttribute('data-visible');
       if (menuVisibility === 'false') {
         primaryNavigation.setAttribute('data-visible', true);
+        mobileMenuToggle.setAttribute('aria-expanded', true);
       } else {
         primaryNavigation.setAttribute('data-visible', false);
+        mobileMenuToggle.setAttribute('aria-expanded', false);
       }
     });
     navLinkMenu.forEach((element) => {
       element.addEventListener('click', () => {
         primaryNavigation.setAttribute('data-visible', false);
+        mobileMenuToggle.setAttribute('aria-expanded', false);
       });
     });
   }
