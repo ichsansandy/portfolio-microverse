a3842c0a7f1064fd1a2b860336c2bfea4b641dda
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-05-15 17:52:58 +0700

    Adding scroll smooth and prevent user for scrolling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
   const mobileMenuToggle = document.querySelector('.mobile-menu-toggle');
   const primaryNavigation = document.querySelector('#primary-navigation');
   const navLinkMenu = document.querySelectorAll('.nav-link');
+  const body = document.querySelector('body');
 
   if (mobileMenuToggle) {
     mobileMenuToggle.addEventListener('click', () => {
@@ -9,15 +10,18 @@ window.addEventListener('DOMContentLoaded', () => {
       if (menuVisibility === 'false') {
         primaryNavigation.setAttribute('data-visible', true);
         mobileMenuToggle.setAttribute('aria-expanded', true);
+        body.classList.add('prevent-scrolling');
       } else {
         primaryNavigation.setAttribute('data-visible', false);
         mobileMenuToggle.setAttribute('aria-expanded', false);
+        body.classList.remove('prevent-scrolling');
       }
     });
     navLinkMenu.forEach((element) => {
       element.addEventListener('click', () => {
         primaryNavigation.setAttribute('data-visible', false);
         mobileMenuToggle.setAttribute('aria-expanded', false);
+        body.classList.remove('prevent-scrolling');
       });
     });
   }
